3c34455c125550e3c2be938d645cb42016d7df36
QueenJammie <jammie.lacc@outlook.com>
2022-05-29 19:56:04 -0400

    Remove userless images and the farenheit conversion

--- src/script.js
+++ src/script.js
@@ -129,7 +129,7 @@
         hourElement.innerHTML = hour + "h";
         let iconElement = document.querySelector("#icon");
         iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-        iconElement.setAttribute("alt", `${response.data.weather[0].description}`)
+        iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
         let temperatureElement = document.querySelector("#temperature-element");
         temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
